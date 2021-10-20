5bd94c052d7d905d19dc33f9ccecfc2dcb81819a
oeyoews <2956398608@qq.com>
2021-10-20 23:04:20 +0800

    try to use lua to refactor

--- after/plugin/other.lua
+++ after/plugin/other.lua
@@ -1,3 +1,5 @@
+vim.cmd[[
+
 " 1. vim-pug
 nnoremap <Leader>vi <Cmd>PlugInstall<CR>
 nnoremap <Leader>vc <Cmd>PlugClean<CR>
@@ -32,3 +34,4 @@ let g:mkdp_open_to_the_world = 0
 let g:mkdp_filetypes = [ 'markdown', 'html' ]
 
 nnoremap <space>fmm :MarkdownPreviewToggle<CR>
+]]
