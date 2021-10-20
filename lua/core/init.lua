0566f328c7dbfe1cc8e0d870abf3a4e260dfe4bc
oeyoews <2956398608@qq.com>
2021-10-20 23:25:25 +0800

    🌛 to use lua

---
+++ lua/core/init.lua
@@ -0,0 +1,9 @@
+require("core.bufferline")
+require("core.luadev")
+require("core.statusline")
+require("core.treesitter")
+require("core.lspconfig")
+require("core.other")
+require("core.telescope")
+require("core.which-key")
+
