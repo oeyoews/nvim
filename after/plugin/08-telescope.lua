851ce9fdfe9b9bc2344e7d4f041c3a4e1be72e9f
oeyoews <jyao4783@gmail.com>
2022-06-09 20:32:43 +0800

    pref: remove install.sh and modify .editorconfig

--- after/plugin/08-telescope.lua
+++ after/plugin/08-telescope.lua
@@ -67,26 +67,7 @@ telescope.setup {
       theme = "ivy"
     }
   },
-
-  extensions = {
-    --fzf = {
-    --fuzzy = true,                    -- false will only do exact matching
-    --override_generic_sorter = true,  -- override the generic sorter
-    --override_file_sorter = true,     -- override the file sorter
-    --case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-    ---- the default case_mode is "smart_case"
-    --},
-    --fzy_native = {
-    --override_generic_sorter = false,
-    --override_file_sorter = true,
-    --}
-
-  },
-
 }
-
---require('telescope').load_extension('fzf')
---require('telescope').load_extension('fzy_native')
 
 vim.cmd [[
 nnoremap <leader>ff <cmd>Telescope find_files <cr>
