c07f277d0dc3bc98744dfb979b6fdb293e355010
oeyoews <jyao4783@gmail.com>
2022-06-13 11:06:13 +0800

    pref: adjust vertsplit color

--- custom/config/command.vim
+++ custom/config/command.vim
@@ -11,6 +11,7 @@ autocmd! FileType lspinfo,startuptime,help,qf,quickrun,snippets,tsplayground nno
 
 " autoload refresh custom/config/ all files, when they changed, not use echo
 " message
-autocmd! BufWritePost ~/.config/nvim/custom/config/*.vim source %
+" autocmd! BufWritePost ~/.config/nvim/custom/config/*.vim source %
+autocmd! BufWritePost *.vim source %
 
 autocmd! FileType lua nnoremap <silent> <leader>lfl <cmd>!stylua %<cr>
--- lua/config/themes/tokyonight.lua
+++ lua/config/themes/tokyonight.lua
@@ -1,9 +1,8 @@
----[[
 local status_ok, tokyonight = pcall(require, 'tokyonight')
 
 if not status_ok then
-	vim.notify('Theme not found')
-	return false
+  vim.notify('Theme not found')
+  return false
 end
 
 tokyonight.colorscheme()
@@ -21,25 +20,22 @@ hi PmenuSel     ctermfg=242 ctermbg=0 guifg=#282c34 guibg=#73b8f1
 hi PmenuThumb ctermfg=160 ctermbg=97 guifg=#282c34 guibg=#73b8f1
 
 " #FFCC00
-hi Cursor guifg=#51afef guibg=#51afef
-hi Cursor2 guifg=red guibg=red
 hi IncSearch  cterm=bold ctermfg=16 ctermbg=167 guifg=#292b2e guibg=#ce537a
 hi Search     ctermfg=16 ctermbg=76 guifg=#292b2e guibg=#86dc2f
-hi Keyword    cterm=bold ctermfg=68 guifg=#8167a3 gui=bold
-hi CursorLineNr      guifg=#51afef guibg=#000000
-hi SpellBad   ctermbg=9 gui=underline guifg=#f7768e
-hi VertSplit guibg=yellow
+hi CursorLineNr      gui=none guibg=none
+hi VertSplit guibg=none guifg=#353b45 gui=bold cterm=reverse
+hi Keyword   guifg=#c678dd gui=bold
 
 " set cursor color
-set guicursor=n-v-c:block-Cursor/lCursor,i-ci-ve:ver25-Cursor2/lCursor2,r-cr:hor20,o:hor50
+hi Cursorn guifg=none guibg=none
+hi Cursori guifg=red guibg=red
+set guicursor=n-v-c:block-Cursorn/lCursorn,i-ci-ve:ver25-Cursori/lCursori,r-cr:hor20,o:hor50
 
 " yank highlight
 autocmd TextYankPost * lua vim.highlight.on_yank {higroup="IncSearch",
-      \ timeout=150, on_visual=true}
+      \ timeout=550, on_visual=true}
 
 " restore the cursor location
 autocmd! BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
 
 ]])
-
---]]
