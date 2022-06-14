d973709a4fc73eb9ab765c4ea5efc6d39d76d6db
oeyoews <jyao4783@gmail.com>
2022-06-14 11:18:56 +0800

    fix: disable gitlint

--- lua/plugins/lsp/null-ls.lua
+++ lua/plugins/lsp/null-ls.lua
@@ -2,19 +2,19 @@ local ok, null_ls = pcall(require, 'null-ls')
 local builtin = null_ls.builtins
 
 if not ok then
-    return false
+  return false
 end
 
 null_ls.setup({
-    debounce = 500,
-    sources = {
-        -- builtin.formatting.stylua,
-        -- builtin.diagnostics.codespell, -- need install codespell
-        -- builtin.formatting.prettierd.with,
-        -- builtin.diagnostics.misspell,
-        builtin.diagnostics.markdownlint, -- need install markdownlint
-        builtin.diagnostics.gitlint, -- need install markdownlint
-        -- builtin.completion.spell,
-        -- builtin.code_actions.shellcheck
-    },
+  debounce = 500,
+  sources = {
+    -- builtin.formatting.stylua,
+    -- builtin.diagnostics.codespell, -- need install codespell
+    -- builtin.formatting.prettierd.with,
+    -- builtin.diagnostics.misspell,
+    builtin.diagnostics.markdownlint, -- need install markdownlint
+    -- builtin.diagnostics.gitlint, -- need install markdownlint
+    -- builtin.completion.spell,
+    -- builtin.code_actions.shellcheck
+  },
 })
