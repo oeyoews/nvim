216f1dd6deaa6acfb92954fc47108dac3b0a7371
oeyoews <jyao4783@gmail.com>
2022-06-15 01:40:31 +0800

    test: add packer error settings

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,4 +1,27 @@
----[[
+local fn = vim.fn
+
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({
+    'git',
+    'clone',
+    '--depth',
+    '1',
+    'https://github.com/wbthomason/packer.nvim',
+    install_path
+  })
+end
+
+local ok, packer = pcall(require, 'packer')
+
+if not ok then
+  vim.notify('packer not founded')
+  return false
+end
+
+
+--[[
 vim.cmd [[
 " First install setting steps
 let data_dir = stdpath('data') . '/site'
