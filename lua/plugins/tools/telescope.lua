69771c0f39d81fa94f86962234d9346648449124
oeyoews <jyao4783@gmail.com>
2022-06-16 00:22:49 +0800

    pref: shorten telescope config theme

--- lua/plugins/tools/telescope.lua
+++ lua/plugins/tools/telescope.lua
@@ -7,28 +7,32 @@ end
 
 local actions = require("telescope.actions")
 
+local theme = "dropdown"
+
 -- disable preview binaries
 local previewers = require("telescope.previewers")
+
 local Job = require("plenary.job")
+
 local new_maker = function(filepath, bufnr, opts)
   filepath = vim.fn.expand(filepath)
   Job
-    :new({
-      command = "file",
-      args = { "--mime-type", "-b", filepath },
-      on_exit = function(j)
-        local mime_type = vim.split(j:result()[1], "/")[1]
-        if mime_type == "text" then
-          previewers.buffer_previewer_maker(filepath, bufnr, opts)
-        else
-          -- maybe we want to write something to the buffer here
-          vim.schedule(function()
-            vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "BINARY" })
-          end)
-        end
-      end,
-    })
-    :sync()
+      :new({
+        command = "file",
+        args = { "--mime-type", "-b", filepath },
+        on_exit = function(j)
+          local mime_type = vim.split(j:result()[1], "/")[1]
+          if mime_type == "text" then
+            previewers.buffer_previewer_maker(filepath, bufnr, opts)
+          else
+            -- maybe we want to write something to the buffer here
+            vim.schedule(function()
+              vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "BINARY" })
+            end)
+          end
+        end,
+      })
+      :sync()
 end
 
 telescope.setup({
@@ -43,31 +47,32 @@ telescope.setup({
     entry_prefix = "  ",
   },
 
+  -- FIXME: to shorten it
   pickers = {
     find_files = {
-      theme = "dropdown",
+      theme = theme,
       --preview = false,
     },
     oldfiles = {
-      theme = "dropdown",
+      theme = theme,
     },
     buffers = {
-      theme = "dropdown",
+      theme = theme,
     },
     live_grep = {
-      theme = "dropdown",
+      theme = theme,
     },
     colorscheme = {
-      theme = "ivy",
+      theme = theme,
     },
     keymaps = {
-      theme = "ivy",
+      theme = theme,
     },
     filetypes = {
-      theme = "dropdown",
+      theme = theme,
     },
     current_buffer_fuzzy_find = {
-      theme = "ivy",
+      theme = theme,
     },
   },
 })
