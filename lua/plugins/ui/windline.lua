b091789f1c7c5aa6520ed63a6169d074412f433e
oeyoews <jyao4783@gmail.com>
2022-06-16 01:05:27 +0800

    🍺 This is an automatic push

--- lua/plugins/ui/windline.lua
+++ lua/plugins/ui/windline.lua
@@ -1,10 +1,9 @@
-local ok, _ = pcall(require, "wlsample")
+local ok, _ = pcall(require, "windline")
 
 if not ok then
   vim.notify("windline not founded")
   return false
 end
 
+-- option bubble(2)
 require('wlsample.evil_line')
--- require('wlsample.bubble')
--- require('wlsample.bubble2')
