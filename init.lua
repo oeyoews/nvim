eea6ec24b2d0d391fd4c8a21e53bd3fdb471b374
oeyoews <jyao4783@gmail.com>
2022-06-21 00:43:01 +0800

    🎸 This is an automatic push

--- init.lua
+++ init.lua
@@ -1,11 +1,1 @@
 require("core")
-
-
-vim.wo.foldcolumn = '1'
-vim.wo.foldlevel = 99 -- feel free to decrease the value
-vim.wo.foldenable = true
-
-require('ufo').setup()
--- vim.cmd([[
--- hi default link UfoFoldedEllipsis Comment
--- ]])
