20ddca0fd04ab4c4beaa137d55274d937a7d768b
oeyoews <jyao4783@gmail.com>
2022-06-20 16:24:32 +0800

    🎸 This is an automatic push

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+# just for arch series
 sudo pacman -S ranger python-neovim nodejs go python shellcheck make noto-fonts-emoji neovim -y
--- lua/plugins/lsp/null_ls.lua
+++ lua/plugins/lsp/null_ls.lua
@@ -23,6 +23,7 @@ local enabale_filetypes = require("plugins.lsp.filetypes").filetypes
 -- table.insert(enabale_filetypes, "gitcommit")
 -- ref: http://static.kancloud.cn/kancloud/lua_style_guide/66331
 enabale_filetypes[#enabale_filetypes + 1] = "gitcommit"
+enabale_filetypes[#enabale_filetypes + 1] = "sh"
 
 local sources = {
   -- builtin.diagnostics.gitlint, -- need install gitlint
