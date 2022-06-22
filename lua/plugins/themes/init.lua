34516108c157f3a3575debedfbcbf8450e63b23a
oeyoews <jyao4783@gmail.com>
2022-06-22 11:40:43 +0800

    🧋 This is an automatic push

--- lua/plugins/themes/init.lua
+++ lua/plugins/themes/init.lua
@@ -12,12 +12,8 @@ for _, load_module in ipairs(modules) do
   if not status_ok then
     -- vim.api.nvim_err_writeln("Failed to load " .. load_module .. "\n\n" .. fault)
     local plugin = "Modules"
-    vim.notify(
-      "Failed to load " .. load_module,
-      "warn",
-      {
-        title = plugin,
-      }
-    )
+    vim.notify("Failed to load " .. load_module, "warn", {
+      title = plugin,
+    })
   end
 end
