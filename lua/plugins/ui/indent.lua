84ff86d602902b3da4a94b797d02d26c349f0380
oeyoews <jyao4783@gmail.com>
2022-06-24 00:38:09 +0800

    🧋 This is an automatic push

--- lua/plugins/ui/indent.lua
+++ lua/plugins/ui/indent.lua
@@ -1,17 +1,5 @@
-local ok, indent_blankline = pcall(require, "indent_blankline")
+local ok, _ = pcall(require, "indent_blankline")
 
 if not ok then
   vim.notify("indent_blankline not founded")
 end
-
-indent_blankline.setup({
-  show_current_context = true,
-  show_current_context_start = true,
-})
-
--- #E06C75
--- #E5C07B
--- #98C379
--- #56B6C2
--- #61AFEF
--- #C678DD
