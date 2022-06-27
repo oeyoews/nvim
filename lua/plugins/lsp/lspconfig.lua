59ad1bdc65316ba8c7749bd778fee99622a71120
oeyoews <jyao4783@gmail.com>
2022-06-27 14:07:51 +0800

    🧋 This is an automatic push

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -71,16 +71,16 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagn
   },
 })
 
-require 'lspconfig'.sumneko_lua.setup {
+require("lspconfig").sumneko_lua.setup({
   settings = {
     Lua = {
       runtime = {
         -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
+        version = "LuaJIT",
       },
       diagnostics = {
         -- Get the language server to recognize the `vim` global
-        globals = { 'vim' },
+        globals = { "vim" },
       },
       workspace = {
         -- Make the server aware of Neovim runtime files
@@ -92,4 +92,4 @@ require 'lspconfig'.sumneko_lua.setup {
       },
     },
   },
-}
+})
