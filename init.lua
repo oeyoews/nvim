b5e9ce7a3f602208a470447ce44d51bebe4ade20
oeyoews <jyao4783@gmail.com>
2022-06-28 23:12:20 +0800

    🧋 This is an automatic push

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@
 --  ┃CHECK NVIM VERSION┠──►┃ENABLE NIGHT FEATURE┠──►┃LOAD PLUGIN MODULES┃
 --  ┗━━━━━━━━━━━━━━━━━━*   ┗━━━━━━━━━━━━━━━━━━━━*   ┗━━━━━━━━━━━━━━━━━━━*
 --]]
+
 if vim.fn.has("nvim-0.7") ~= 1 then
   vim.notify("   Please update your neovim to latest")
   return
