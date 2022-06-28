4b9cb7145e76d5d4fff5f33dbeb76760e20bc021
oeyoews <jyao4783@gmail.com>
2022-06-28 12:09:27 +0800

    🧋 This is an automatic push

--- lua/modules/lsp/treesitter.lua
+++ lua/modules/lsp/treesitter.lua
@@ -2,6 +2,7 @@ local ok, nvim_treesitter_configs = pcall(require, "nvim-treesitter.configs")
 
 if not ok then
   vim.notify("  tree_configs not founded")
+  return
 end
 
 -- @Mirror_nvim_treesitter
