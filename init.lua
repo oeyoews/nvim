009533e5019c41ddc4588e8877291b3a24c72663
oeyoews <jyao4783@gmail.com>
2022-06-28 23:08:11 +0800

    🧋 This is an automatic push

--- init.lua
+++ init.lua
@@ -1,24 +1,32 @@
--- ┏━━━━━━━━━━━━━━━━━━┓
--- ┃CHECK NVIM VERSION┃
--- ┗━━━━━━━━━━━━━━━━━━┛
+--[[
+  ┏━━━━━━━━━━━━━━━━━━┓
+  ┃CHECK NVIM VERSION┃
+  ┗━━━━━━━━━━━━━━━━━━┛
+--]]
 if vim.fn.has("nvim-0.7") ~= 1 then
   vim.notify("   Please update your neovim to latest")
   return
 end
 
--- ┏━━━━━━━━━━━━━━━━━━━━┓
--- ┃ENABLE NIGHT FEATURE┃
--- ┗━━━━━━━━━━━━━━━━━━━━┛
+--[[
+  ┏━━━━━━━━━━━━━━━━━━━━┓
+  ┃ENABLE NIGHT FEATURE┃
+  ┗━━━━━━━━━━━━━━━━━━━━┛
+--]]
 if vim.fn.has("nvim-0.8.0") == 1 then
   vim.opt.cmdheight = 0
 end
 
--- ┏━━━━━━━━━━━━━━━━━━┓
--- ┃NEED ORDER LOADING┃
--- ┗━━━━━━━━━━━━━━━━━━┛
+--[[
+  ┏━━━━━━━━━━━━━━━━━━┓
+  ┃NEED ORDER LOADING┃
+  ┗━━━━━━━━━━━━━━━━━━┛
+--]]
 require("modules.order")
 
--- ┏━━━━━━━━━━━━━━━━━━━┓
--- ┃LOAD PLUGIN MODULES┃
--- ┗━━━━━━━━━━━━━━━━━━━┛
+--[[
+  ┏━━━━━━━━━━━━━━━━━━━┓
+  ┃LOAD PLUGIN MODULES┃
+  ┗━━━━━━━━━━━━━━━━━━━┛
+--]]
 require("modules.utils.pcall").setup(require("modules").modules)
