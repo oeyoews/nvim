a35dd4f203a25683554f7892e8d45249b98d89ce
oeyoews <jyao4783@gmail.com>
2022-07-02 17:05:16 +0800

    🧋 This is an automatic push

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -4,6 +4,8 @@ local M = {}
 
 M.settings = {
   debug_mode = false, -- @ref: core.utils.pcall
+  -- rolling.json default.json v1.0.0.json
+  -- @q how to use this
   snapshot = nil, -- @ref: core.utils.bootstrap
 }
 
