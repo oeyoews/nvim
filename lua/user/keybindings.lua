ecd82aa3bdce8cd88724986ec3c5058c662d76f1
oeyoews <jyao4783@gmail.com>
2022-07-02 14:09:17 +0800

    🧋 This is an automatic push

--- lua/user/keybindings.lua
+++ lua/user/keybindings.lua
@@ -1,13 +1,13 @@
 local M = {}
 
 -- @ref: core.utils.mappings
-function M.findfiles()
+M.findfiles = function()
   vim.cmd([[
  nnoremap <silent> <space>fi <cmd>find ~/.config/nvim/init.lua<cr>
  nnoremap <silent> <space>fp <cmd>find ~/.config/nvim/lua/user/pluginlist.lua<cr>
  nnoremap <silent> <space>fm <cmd>find ~/.config/nvim/lua/user/modules.lua<cr>
  nnoremap <silent> <space>fk <cmd>find ~/.config/nvim/lua/user/keybindings.lua<cr>
-]])
+]] )
 end
 
 return M
