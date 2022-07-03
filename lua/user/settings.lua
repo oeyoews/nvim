77326d67fc1f06f1186416e423b07f64c6322d13
oeyoews <jyao4783@gmail.com>
2022-07-03 18:56:52 +0800

    🧋 This is an automatic push

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -21,25 +21,24 @@ M.sumneko_lua = {
 }
 
 -- NOTE: need close your ladder to connect this website
---[[ M.jsonls = {
-  json = {
-    schemas = require("schemastore").json.schemas(),
-    validate = { enable = true },
-  },
-}
---]]
+-- M.jsonls = {
+--   json = {
+--     schemas = require("schemastore").json.schemas(),
+--     validate = { enable = true },
+--   },
+-- }
 
-M.yamlls = {
-  yaml = {
-    format = { enable = true, singleQuote = true },
-    validate = true,
-    hover = true,
-    completion = true,
-    schemaStore = {
-      enable = true,
-      url = "https://www.schemastore.org/api/json/catalog.json",
-    },
-  },
-}
+-- M.yamlls = {
+--   yaml = {
+--     format = { enable = true, singleQuote = true },
+--     validate = true,
+--     hover = true,
+--     completion = true,
+--     schemaStore = {
+--       enable = true,
+--       url = "https://www.schemastore.org/api/json/catalog.json",
+--     },
+--   },
+-- }
 
 return M
