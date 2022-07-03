50b058acda4750f2b3bff41e0aa1aec2a47f899d
oeyoews <jyao4783@gmail.com>
2022-07-03 16:27:54 +0800

    feat: use random to use random theme

--- lua/core/utils/tokyonight.lua
+++ lua/core/utils/tokyonight.lua
@@ -1,14 +1,18 @@
--- feature: toggle theme night and storm
-local function switch_theme_ai()
-  local h = tonumber(os.date("%H"))
-  -- default theme is storm in day
+--- switch theme ai
+local switch_theme_ai = function()
+  -- @bug: https://stackoverflow.com/questions/20154991/generating-uniform-random-numbers-in-lua
+  math.randomseed(os.time())
   local theme = "storm"
-
+  local h = tonumber(os.date("%H"))
+  local status = math.random(0, 1)
+  -- default theme is storm in day
   -- toggle night theme in nighttime
   if h < 8 or h > 20 then
+    -- if h > 8 and h < 20 then
+    theme = "night"
+  elseif status == 1 then
     theme = "night"
   end
-
   vim.g.tokyonight_style = theme
   require("tokyonight").setup()
 end
