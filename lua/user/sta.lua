f9e2618cc8030c79e9d15089ddba395a9df48ff3
oeyoews <jyao4783@gmail.com>
2022-07-03 22:13:21 +0800

    pref: less code for sta.lua

--- lua/user/sta.lua
+++ lua/user/sta.lua
@@ -10,10 +10,7 @@ local sta = function(opt)
   local status = math.random(0, 1)
   -- default theme is storm in day
   -- toggle night theme in nighttime
-  if hour_number < 8 or hour_number > 20 then
-    -- if h > 8 and h < 20 then
-    theme = "night"
-  elseif status == 1 then
+  if hour_number < 8 or hour_number > 20 or status == 1 then
     theme = "night"
   end
   vim.g.tokyonight_style = theme
