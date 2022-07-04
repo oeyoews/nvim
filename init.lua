72e6d13fd3591e24c7bf06e90a772e5e534c4a95
oeyoews <jyao4783@gmail.com>
2022-07-04 23:45:12 +0800

    pref: move user.init.lua to replace init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,8 @@
-require("user")
+local modules = require("user.modules").modules
+-- modules config
+local m3 = require("user.modules").m3
+
+-- load core.utils
+require("user.pcall").setup("core", { "utils" }, m3)
+-- load modules
+require("user.pcall").setup("modules", modules, m3)
--- lua/user/init.lua
+++
@@ -1,7 +0,0 @@
-local entry = "modules"
-local modules = require("user.modules").modules
-local m3 = require("user.modules").m3
-
-require("user.pcall").setup("core", { "utils" }, m3)
-
-require("user.pcall").setup(entry, modules, m3)
