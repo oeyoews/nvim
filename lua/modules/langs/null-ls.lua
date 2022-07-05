1efc0e6e159ba0773b1c1a6d8e9e7906995c4049
oeyoews <jyao4783@gmail.com>
2022-07-05 18:33:13 +0800

    🧋 This is an automatic push

--- lua/modules/langs/null-ls.lua
+++ lua/modules/langs/null-ls.lua
@@ -17,15 +17,10 @@ local disabled_filetypes = {
 -- @parsers.lua
 local enabale_filetypes = require("user.parsers")
 
--- enabale_filetypes = enabale_filetypes.parsers
-
--- ref: http://static.kancloud.cn/kancloud/lua_style_guide/66331
-enabale_filetypes[#enabale_filetypes + 1] = "sh"
-enabale_filetypes[#enabale_filetypes + 1] = "gitcommit"
-
 local sources = {
-  diagnostics.codespell,
-  completion.spell,
+  -- completion.spell, -- ugly
+  -- diagnostics.codespell,
+  diagnostics.codespell.with({}),
   -- @markdown
   -- diagnostics.markdownlint.with({
   --   filetypes = {
@@ -37,33 +32,30 @@ local sources = {
   -- diagnostics.eslint, -- need install eslint
   -- diagnostics.standardjs, -- need install eslint
   -- need install codespell
-  -- diagnostics.codespell.with({
-  --   disabled_filetypes = disabled_filetypes,
-  --   filetypes = enabale_filetypes,
-  -- }),
 }
 
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+-- autoformatlly format
+-- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          -- vim.lsp.buf.format({
-          --   bufnr = bufnr,
-          --   filter = function(client)
-          --     return client.name == "null-ls"
-          --   end
-          -- })
-          vim.lsp.buf.formatting_sync()
-        end,
-      })
-    end
-  end,
+  -- on_attach = function(client, bufnr)
+  --   if client.supports_method("textDocument/formatting") then
+  --     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+  --     vim.api.nvim_create_autocmd("BufWritePre", {
+  --       group = augroup,
+  --       buffer = bufnr,
+  --       callback = function()
+  --         -- vim.lsp.buf.format({
+  --         --   bufnr = bufnr,
+  --         --   filter = function(client)
+  --         --     return client.name == "null-ls"
+  --         --   end
+  --         -- })
+  --         vim.lsp.buf.formatting_sync()
+  --       end,
+  --     })
+  --   end
+  -- end,
   update_in_insert = false,
   debounce = 500,
   sources = sources,
