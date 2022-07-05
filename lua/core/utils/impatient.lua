fd587bba23c11862d6163ba59fe99da3a27f5272
oeyoews <jyao4783@gmail.com>
2022-07-05 17:48:08 +0800

    fix: rewrite filetype.lua(impatient)

--- lua/core/utils/impatient.lua
+++ lua/core/utils/impatient.lua
@@ -5,9 +5,9 @@ local filetype_ok, filetype = pcall(require, "filetype")
 if filetype_ok then
   filetype.setup({
     overrides = {
-      complex = {
-        [".service"] = "systemd",
-        [".info"] = "json",
+      extensions = {
+        service = "systemd",
+        info = "json",
       },
     },
   })
