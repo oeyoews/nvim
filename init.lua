6bd64c9ae9e27d45b90e56db74059f4c36998fda
oeyoews <jyao4783@gmail.com>
2022-07-27 11:41:28 +0800

    🧋 This is an automatic push

--- init.lua
+++ init.lua
@@ -2,8 +2,9 @@
 local files = require("user.modules").files
 local modules = require("user.modules").modules
 local utils = require("user.modules").utils
+local pcall = require("user.pcall")
 
 -- ﱣ load core.utils
-require("user.pcall").setup("core", utils, files)
+pcall.setup("core", utils, files)
 -- ﱤ load modules
-require("user.pcall").setup("modules", modules, files)
+pcall.setup("modules", modules, files)
