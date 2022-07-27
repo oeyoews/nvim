bc96b4cdfe8672f9659ea08f09c8eed3918859e0
oeyoews <jyao4783@gmail.com>
2022-07-27 21:33:27 +0800

    robot: This is an automatic push

--- lua/modules/utils/bootstrap.lua
+++ lua/modules/utils/bootstrap.lua
@@ -11,7 +11,7 @@ end
 --- install packer.nvim firstly
 if fn.empty(fn.glob(install_path)) > 0 then
   -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e222a" })
-  vim.notify([[   You have not inistall packer.nvim  Cloning packer.nvim ...]], "info")
+  vim.notify([[   You have not inistall packer.nvim  Cloning packer.nvim ...]], "info")
   packer_bootstrap = fn.system({
     "git",
     "clone",
