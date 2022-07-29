2b7de553735b7e2e00e162a2734add4575be0002
oeyoews <jyao4783@gmail.com>
2022-07-29 22:08:39 +0800

    robot: This is an automatic push

--- lua/modules/tools/icon_picker.lua
+++ lua/modules/tools/icon_picker.lua
@@ -1,4 +1,16 @@
-require("icon-picker")
+require("icon-picker").setup({
+  disable_legacy_commands = false,
+})
 
-vim.keymap.set("i", "<A-i>", "<cmd>PickIconsInsert<cr>", { desc = "𝑷 insert icon" })
-vim.keymap.set("n", "<space>ie", "<cmd>PickIcons<cr>", { desc = "𝑷 insert icon" })
+vim.keymap.set(
+  "i",
+  "<A-i>",
+  "<cmd>IconPickerNormal alt_font symbols nerd_font emoji<cr>",
+  { desc = "𝑷 insert icon" }
+)
+vim.keymap.set(
+  "n",
+  "<space>ie",
+  "<cmd>IconPickerNormal alt_font symbols nerd_font emoji<cr>",
+  { desc = "𝑷 insert icon" }
+)
