2517b8d009d6de088b9ec0d9047887bec6925e7f
oeyoews <jyao4783@gmail.com>
2022-07-30 16:23:02 +0800

    robot: This is an automatic push

--- lua/user/fun.lua
+++ lua/user/fun.lua
@@ -26,13 +26,23 @@ M.get_neovim_info = function()
   local total_plugins = #vim.tbl_keys(packer_plugins)
   local version = vim.version()
   local nvim_version_info = string.format("%s.%s.%s", version.major, version.minor, version.patch)
-  return vim.notify(
-    string.format("  %s plugins,  %s,  %s ", total_plugins, nvim_version_info, get_tag()[1]),
-    "info",
-    {
-      title = "neovim info",
-    }
+  local fmt_msg = string.format(
+    [[
+ #  You installed `%s` plugins by packer
+ #  Your neovim version: `%s`
+ #  Your personal configuation version: `%s` ]],
+    total_plugins,
+    nvim_version_info,
+    get_tag()[1]
   )
+  return vim.notify(fmt_msg, "info", {
+    title = "neovim info",
+    timeout = 3000,
+    on_open = function(win)
+      local buf = vim.api.nvim_win_get_buf(win)
+      vim.api.nvim_buf_set_option(buf, "filetype", "markdown")
+    end,
+  })
 end
 
 --- creat_journey
