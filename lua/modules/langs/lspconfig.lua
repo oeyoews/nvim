735ae08973518d0ff0105451cc9fda278bbe14c2
oeyoews <jyao4783@gmail.com>
2022-07-31 18:24:46 +0800

    robot: This is an automatic push

--- lua/modules/langs/lspconfig.lua
+++ lua/modules/langs/lspconfig.lua
@@ -92,8 +92,12 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagn
 })
 
 -- change icons
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+local custom_icon = function()
+  local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+  for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+  end
 end
+
+custom_icon()
