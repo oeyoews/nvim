a31786fac422fef9c70bbfb8bf188ab66c332ff5
oeyoews <jyao4783@gmail.com>
2022-08-01 11:58:39 +0800

    robot: This is an automatic push

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@
 --   By: oeyoews <jyao4783@gmail.com>                                         --
 --                                                                            --
 --   Created: 2022/08/01 00:49:04 by oeyoews                                  --
---   Updated: 2022/08/01 11:41:25 by oeyoews                                  --
+--   Updated: 2022/08/01 11:58:11 by oeyoews                                  --
 --                                                                            --
 -- -------------------------------------------------------------------------- --
 
@@ -19,11 +19,28 @@ local modules = Config.modules
 -- ﱤ load modules
 pcall.setup("modules", modules, files)
 
-if oeyoews.nvim_version < 8 then
-  local version_msg = string.format(
-    [[
- ⚠ Just support neovim %s or later]],
-    oeyoews.nvim_full_version_info
-  )
-  vim.notify(version_msg, "warn")
+local check_version = function()
+  if oeyoews.nvim_version < 8 then
+    local version_msg = string.format(
+      [[
+ ⚠ Your neovim version is %s， please install neovim 0.8.0 or later]],
+      oeyoews.nvim_full_version_info
+    )
+    vim.notify(version_msg, "warn")
+  end
 end
+
+local autocmd = vim.api.nvim_create_autocmd
+
+-- Don't auto commenting new lines
+autocmd("BufEnter", {
+  pattern = "*",
+  command = "set fo-=c fo-=r fo-=o",
+})
+
+autocmd("FileType", {
+  pattern = "packer",
+  callback = function()
+    check_version()
+  end,
+})
