513d757b01ffdb4ae0e3ef18f0d8b6c8e0811e68
oeyoews <jyao4783@gmail.com>
2022-08-02 15:17:07 +0800

    refactor(neorg): use require config in ftplugin

--- lua/modules/tools/hop.lua
+++ lua/modules/tools/hop.lua
@@ -1,5 +1,11 @@
 require("hop").setup()
 
-vim.keymap.set("n", "<space>hw", "<cmd>HopWord<cr>", { desc = "𝓗 hop word" })
-vim.keymap.set("n", "<space>hl", "<cmd>HopLine<cr>", { desc = "𝓗 hop line" })
-vim.keymap.set("n", "<space>ha", "<cmd>HopAnywhere<cr>", { desc = "𝓗 hop Anywhere" })
+vim.keymap.set("n", "<space>hw", "<cmd>HopWord<cr>", {
+  desc = "𝓗 hop word",
+})
+vim.keymap.set("n", "<space>hl", "<cmd>HopLine<cr>", {
+  desc = "𝓗 hop line",
+})
+vim.keymap.set("n", "<space>ha", "<cmd>HopAnywhere<cr>", {
+  desc = "𝓗 hop Anywhere",
+})
--- lua/modules/tools/mkdp.lua
+++ lua/modules/tools/mkdp.lua
@@ -1,16 +1,19 @@
-local g = vim.g
-
-g.mkdp_auto_close = 0
-g.mkdp_echo_preview_url = false
-g.mkdp_port = 9999
-g.mkdp_open_to_the_world = false -- need network
-g.mkdp_theme = "dark"
-
-g.mkdp_filetypes = {
-  "markdown",
-  "txt",
-  "html",
+local mkdp_options = {
+  mkdp_auto_close = 0,
+  mkdp_echo_preview_url = false,
+  mkdp_port = 9999,
+  mkdp_open_to_the_world = false, -- need network
+  mkdp_theme = "dark",
+  mkdp_filetypes = {
+    "markdown",
+    "txt",
+    "html",
+  },
 }
+
+for index, value in pairs(mkdp_options) do
+  vim.g[index] = value
+end
 
 vim.keymap.set("n", "<space>fM", "<cmd>MarkdownPreviewToggle<cr>", {
   desc = " preview markdown in browser",
