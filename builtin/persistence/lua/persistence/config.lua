1535e810d55fb6b696e80a486ef535b50240912d
oeyoews <jyao4783@gmail.com>
2022-08-04 23:52:52 +0800

    Automatic commit

--- builtin/persistence/lua/persistence/config.lua
+++ builtin/persistence/lua/persistence/config.lua
@@ -3,13 +3,15 @@ local M = {}
 ---@class PersistenceOptions
 local defaults = {
   -- dir = vim.fn.stdpath("data") .. "/sessions/",
-  dir = "/tmp/sessions/",
+  dir = "/tmp/neovim_sessions/",
   options = { "buffers", "curdir", "tabpages", "winsize" }, -- sessionoptions used for saving
 }
 
 ---@type PersistenceOptions
 M.options = {}
 
+--- setup
+---@param opts string
 function M.setup(opts)
   M.options = vim.tbl_deep_extend("force", {}, defaults, opts or {})
   vim.fn.mkdir(M.options.dir, "p")
