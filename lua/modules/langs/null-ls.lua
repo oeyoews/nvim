ae14452fbc12d1b834e2f2bc534808d758219ce9
oeyoews <jyao4783@gmail.com>
2022-08-05 23:36:01 +0800

    fix(null-ls): add disabled_filetypes

--- lua/modules/langs/null-ls.lua
+++ lua/modules/langs/null-ls.lua
@@ -7,7 +7,11 @@ local formatting = null_ls.builtins.formatting
 local code_actions = null_ls.builtins.code_actions
 
 local sources = {
-  code_actions.gitsigns,
+  code_actions.gitsigns.with({
+    disabled_filetypes = {
+      "gitcommit",
+    },
+  }),
   -- code_actions.proselint, -- not useful
   -- diagnostics.proselint, -- some time have error tips
   formatting.stylua, -- this is conflict for lsp, choice
