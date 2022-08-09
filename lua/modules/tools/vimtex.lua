4ea2e6c9043c4697ec2bbef01a3edc61b9087b88
oeyoews <jyao4783@gmail.com>
2022-08-09 11:20:10 +0800

    feat(tex): add support cn snip

--- lua/modules/tools/vimtex.lua
+++ lua/modules/tools/vimtex.lua
@@ -4,8 +4,12 @@ vim.g.tex_flavor = "latex"
 -- zathura support preview refresh , but slow
 vim.g.vimtex_view_method = "zathura"
 vim.g.vimtex_view_general_viewer = "okular"
-vim.g.vimtex_quickfix_mode = false
+vim.g.vimtex_quickfix_mode = true
 vim.g.tex_conceal = "abdmg"
 
 -- preview plugin
 -- vim.g.livepreview_cursorhold_recompile = false
+
+-- help docs
+-- zathura link default keys
+-- ref: https://defkey.com/zathura-shortcuts
