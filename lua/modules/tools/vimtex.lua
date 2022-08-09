79013aa9ead86ce70620935314cb474b6c0f292f
oeyoews <jyao4783@gmail.com>
2022-08-09 12:03:15 +0800

    Automatic commit

--- lua/modules/tools/vimtex.lua
+++ lua/modules/tools/vimtex.lua
@@ -4,8 +4,11 @@ vim.g.tex_flavor = "latex"
 -- zathura support preview refresh , but slow
 vim.g.vimtex_view_method = "zathura"
 vim.g.vimtex_view_general_viewer = "okular"
-vim.g.vimtex_quickfix_mode = true
+vim.g.vimtex_quickfix_mode = false
 vim.g.tex_conceal = "abdmg"
+vim.g.vimtex_compiler_latexmk_engines = {
+  ["_"] = "-xelatex",
+}
 
 -- preview plugin
 -- vim.g.livepreview_cursorhold_recompile = false
@@ -13,3 +16,6 @@ vim.g.tex_conceal = "abdmg"
 -- help docs
 -- zathura link default keys
 -- ref: https://defkey.com/zathura-shortcuts
+
+-- cn
+-- https://wiki.archlinux.org/title/TeX_Live_(%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87)#%E7%BA%B8%E5%BC%A0%E5%A4%A7%E5%B0%8F
