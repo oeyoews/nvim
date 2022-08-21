03d579aada37c27788393da1bf195ba356d68241
oeyoews <jyao4783@gmail.com>
2022-08-21 22:01:55 +0800

    pref: add number for bufferline

--- lua/modules/ui/bufferline.lua
+++ lua/modules/ui/bufferline.lua
@@ -1,4 +1,14 @@
-require("bufferline").setup()
+require("bufferline").setup({
+  options = {
+    --[=[ numbers = function(opts)
+      return string.format("%s.%s", opts.raise(opts.id), opts.lower(opts.ordinal))
+    end, --]=]
+    -- For 2.)8.) - change he order of arguments to change the order in the string
+    numbers = function(opts)
+      return string.format("%s", opts.ordinal)
+    end,
+  },
+})
 
 vim.keymap.set("n", "<space>bp", "<cmd>BufferLinePick<cr>", {
   desc = "ﴳ pick tabline",
