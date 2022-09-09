df32f136353b2ff6cd2b8dbf705b594c4e841b53
oeyoews <jyao4783@gmail.com>
2022-09-10 01:38:23 +0800

    Automatic commit

--- lua/modules/utils/pluginlist.lua
+++ lua/modules/utils/pluginlist.lua
@@ -6,7 +6,7 @@
 --   By: oeyoews <jyao4783@gmail.com>                                         --
 --                                                                            --
 --   Created: 2022/08/01 09:49:53 by oeyoews                                  --
---   Updated: 2022/09/07 23:31:14 by oeyoews                                  --
+--   Updated: 2022/09/09 09:08:48 by oeyoews                                  --
 --                                                                            --
 -- -------------------------------------------------------------------------- --
 
@@ -15,6 +15,13 @@
 -- filetypes plugins
 -- plugins list
 oeyoews.pluginlist = {
+  {
+    "max397574/colortils.nvim",
+    cmd = "Colortils",
+    config = function()
+      require("colortils").setup()
+    end,
+  },
   "ibhagwan/fzf-lua",
   "gaoDean/autolist.nvim",
   "nguyenvukhang/nvim-toggler",
