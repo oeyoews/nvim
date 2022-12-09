4c921f907e3652fac2997499399916e2994a0841
oeyoews <jyao4783@gmail.com>
2022-12-09 17:16:51 +0800

    Automatic commit

--- lua/modules/utils/pluginlist.lua
+++ lua/modules/utils/pluginlist.lua
@@ -10,6 +10,18 @@ oeyoews.pluginlist = {
   --   "toppair/peek.nvim",
   --   run = "deno task --quiet build:fast",
   -- },
+  -- "JManch/sunset.nvim",
+  --[=[ {
+    "tamton-aquib/duck.nvim",
+    config = function()
+      vim.keymap.set("n", "<leader>dd", function()
+        require("duck").hatch()
+      end, {})
+      vim.keymap.set("n", "<leader>dk", function()
+        require("duck").cook()
+      end, {})
+    end,
+  }, --]=]
   "nvim-tree/nvim-tree.lua",
   "folke/tokyonight.nvim",
   "rcarriga/nvim-notify",
