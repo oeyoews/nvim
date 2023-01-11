4c434005cdd8f46ac973fae10019afc75a03f473
oeyoews <jyao4783@gmail.com>
2023-01-11 17:28:41 +0800

    perf: more compatiable
    * use function not double vim.cmd

--- lua/modules/utils/pluginlist.lua
+++ lua/modules/utils/pluginlist.lua
@@ -3,171 +3,213 @@
 -- filetypes plugins
 -- plugins list
 oeyoews.pluginlist = {
- {
- "lewis6991/impatient.nvim",
- },
- {
-   "folke/tokyonight.nvim",
-   priority = 1000,
-   config = function()
-     vim.cmd([[colorscheme tokyonight]])
-   end,
- },
- "nvim-tree/nvim-tree.lua",
- "rcarriga/nvim-notify",
- "uga-rosa/ccc.nvim",
- "glepnir/lspsaga.nvim",
- "gaoDean/autolist.nvim",
- "nguyenvukhang/nvim-toggler",
- "lervag/vimtex", -- not support lazyload
- "neovim/nvim-lspconfig",
- {
- "folke/which-key.nvim",
- config = [[require("modules.tools.whichkey")]],
- },
- "oeyoews/rnvimr",
- "cappyzawa/trim.nvim",
- "thinca/vim-quickrun",
- "sukima/vim-tiddlywiki", -- note: this maybe can't load be web browser
- "phaazon/hop.nvim",
- "nathom/filetype.nvim", -- conflict with c filetypes
- "nvim-lua/plenary.nvim",
- "kyazdani42/nvim-web-devicons",
- -- "j-hui/fidget.nvim",
- "folke/neodev.nvim",
- "folke/trouble.nvim",
- {
+  {
+    "lewis6991/impatient.nvim",
+  },
+  {
+    "folke/tokyonight.nvim",
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
+  "nvim-tree/nvim-tree.lua",
+  {
+  "rcarriga/nvim-notify",
+  config = function()
+    require("modules.ui.notify")
+  end,
+  },
+  "uga-rosa/ccc.nvim",
+  "glepnir/lspsaga.nvim",
+  "gaoDean/autolist.nvim",
+  {
+    "nguyenvukhang/nvim-toggler",
+    config = function()
+      require("modules.tools.toggler")
+    end,
+  },
+  "lervag/vimtex", -- not support lazyload
+  {
+    "folke/which-key.nvim",
+    config = function() 
+      require("modules.tools.whichkey")
+    end,
+  },
+  {
+    "oeyoews/rnvimr", -- false
+    lazy = false,
+    config = function()
+      require("modules.tools.ranger")
+    end,
+  },
+  "cappyzawa/trim.nvim",
+  "thinca/vim-quickrun",
+  "sukima/vim-tiddlywiki", -- note: this maybe can't load be web browser
+  "phaazon/hop.nvim",
+  "nathom/filetype.nvim", -- conflict with c filetypes
+  "nvim-lua/plenary.nvim",
+  "kyazdani42/nvim-web-devicons",
+  -- "j-hui/fidget.nvim",
+  "folke/neodev.nvim",
+  "folke/trouble.nvim",
+  {
     "stevearc/dressing.nvim",
-  event = "VeryLazy"
- },
- {
-   "ziontee113/icon-picker.nvim",
-   keys = {
-     "<Space>ie",
-   },
-   config = [[oeyoews.lazyload.emoji()]],
- },
- {
-   "danymat/neogen",
-   cmd = "Neogen",
-   config = [[require("neogen").setup()]],
- },
- {
-   "lukas-reineke/indent-blankline.nvim",
-   config = [[require("modules.ui.indent")]],
- },
- {
- "akinsho/bufferline.nvim",
- config = [[require("modules.ui.bufferline")]],
- },
- {
-   "nvim-treesitter/nvim-treesitter", -- nvim-treesitter bug: checkhealth nvim-treesitter, and execute tsinstall! vim
-   run = ":TSUpdate",
-   dependencies = {
-     "p00f/nvim-ts-rainbow",
-     "nvim-treesitter/nvim-treesitter-refactor",
-   },
- },
- {
-   "williamboman/mason.nvim",
-   dependencies = {
-     "williamboman/mason-lspconfig.nvim",
-     "WhoIsSethDaniel/mason-tool-installer.nvim",
-     "jose-elias-alvarez/null-ls.nvim",
-     "jayp0521/mason-null-ls.nvim",
-   },
- },
- {
-   "hrsh7th/nvim-cmp",
- event = "InsertEnter",
-   dependencies = {
-     "hrsh7th/cmp-nvim-lsp-signature-help",
-     "quangnguyen30192/cmp-nvim-ultisnips",
-     "hrsh7th/cmp-buffer",
-     "SirVer/ultisnips",
-     "honza/vim-snippets",
-     "hrsh7th/cmp-nvim-lsp",
-     "hrsh7th/cmp-path",
-   },
-   config = function()
+    event = "VeryLazy"
+  },
+  {
+    "ziontee113/icon-picker.nvim",
+    keys = {
+      "<Space>ie",
+    },
+    config = function()
+      oeyoews.lazyload.emoji()
+    end,
+  },
+  {
+    "danymat/neogen",
+    cmd = "Neogen",
+    config = function() 
+      require("neogen").setup()
+    end
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    config = function()
+      require("modules.ui.indent")
+    end
+  },
+  {
+    "akinsho/bufferline.nvim",
+    config = function()
+      require("modules.ui.bufferline")
+     end
+  },
+  {
+    "nvim-treesitter/nvim-treesitter", -- nvim-treesitter bug: checkhealth nvim-treesitter, and execute tsinstall! vim
+    run = ":TSUpdate",
+    dependencies = {
+      "p00f/nvim-ts-rainbow",
+      "nvim-treesitter/nvim-treesitter-refactor",
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+      "jose-elias-alvarez/null-ls.nvim",
+      "jayp0521/mason-null-ls.nvim",
+    },
+    config = function() 
+      require("modules.langs.mason")
+    end
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp-signature-help",
+      "quangnguyen30192/cmp-nvim-ultisnips",
+      "hrsh7th/cmp-buffer",
+      "SirVer/ultisnips",
+      "honza/vim-snippets",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-path",
+    },
+    config = function()
       require("modules.langs.cmp")
-      require("modules.langs.lspconfig")
-   end,
- },
- {
-   "folke/todo-comments.nvim",
-   config = [[require("todo-comments").setup()]],
- },
- {
-   "nvim-telescope/telescope.nvim",
-   config = function()
-     require("modules.tools.telescope")
-   end,
-   requires = {
-     "nvim-telescope/telescope-packer.nvim",
-     "nvim-telescope/telescope-file-browser.nvim",
-   },
- },
- {
-   "lewis6991/gitsigns.nvim",
-   config = function()
-     oeyoews.lazyload.gitsigns()
-   end,
- },
- {
-   "numToStr/Comment.nvim",
-   config = [[require("modules.tools.comment")]],
- },
- {
-   "windwp/nvim-autopairs",
-   config = [[require("nvim-autopairs").setup()]],
- },
- {
-   "oeyoews/tabout.nvim",
-   config = [[require("tabout").setup()]],
- },
- {
-   "oeyoews/nvim-colorizer.lua",
-   cmd = "ColorizerToggle",
-   config = [[require("colorizer").setup()]],
- },
- {
-   "dstein64/vim-startuptime",
-   cmd = "StartupTime",
-   -- config = [[vim.g.startuptime_event_width = 30]],
- },
- {
-   "nvim-neorg/neorg",
-   ft = "norg",
-   config = true
- },
- {
-   "ekickx/clipboard-image.nvim",
-   lazy = true
- },
- {
-   "iamcco/markdown-preview.nvim",
-   build = "cd app && npm install",
- },
- {
-   "oeyoews/header42",
-   dev = true,
-   config = [[require("modules.tools.header42")]],
- },
- {
-   "oeyoews/persistence",
-   dev = true,
-   config = [[require("modules.tools.persistence")]],
- },
- {
-   "oeyoews/telescope",
-   dev = true,
- },
- {
-   "barrett-ruth/live-server.nvim",
-   build = 'yarn global add live-server',
-   config = true,
- },
+    end,
+  },
+      {
+        "neovim/nvim-lspconfig",
+        -- config = [[require("modules.langs.lspconfig")]],
+      },
+  {
+    "folke/todo-comments.nvim",
+    config = function() 
+      require("todo-comments").setup()
+    end
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    config = function()
+      require("modules.tools.telescope")
+    end,
+    requires = {
+      "nvim-telescope/telescope-packer.nvim",
+      "nvim-telescope/telescope-file-browser.nvim",
+    },
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      oeyoews.lazyload.gitsigns()
+    end,
+  },
+  {
+    "numToStr/Comment.nvim",
+    config = [[require("modules.tools.comment")]],
+  },
+  {
+    "windwp/nvim-autopairs",
+    config = [[require("nvim-autopairs").setup()]],
+  },
+  {
+    "oeyoews/tabout.nvim",
+    config = [[require("tabout").setup()]],
+  },
+  { 
+    "oeyoews/nvim-colorizer.lua",
+    cmd = "ColorizerToggle",
+    config = [[require("colorizer").setup()]],
+  },
+  {
+    "dstein64/vim-startuptime",
+    cmd = "StartupTime",
+    -- config = [[vim.g.startuptime_event_width = 30]],
+  },
+  {
+    "nvim-neorg/neorg",
+    ft = "norg",
+    config = true
+  },
+  {
+    "ekickx/clipboard-image.nvim",
+    lazy = true
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    build = "cd app && npm install",
+  },
+  {
+    "oeyoews/header42",
+    dev = true,
+    config = [[require("modules.tools.header42")]],
+  },
+  {
+    "oeyoews/lspformat",
+    dev = true,
+    config = [[require("modules.tools.header42")]],
+  },
+  {
+    "oeyoews/persistence",
+    dev = true,
+    config = [[require("modules.tools.persistence")]],
+  },
+  {
+    "oeyoews/telescope",
+    dev = true,
+  },
+  {
+    "barrett-ruth/live-server.nvim",
+    build = 'yarn global add live-server',
+    config = true,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    config = [[require("lualine").setup()]]
+  }
 }
 
 --  mappings
