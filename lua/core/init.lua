90929d86b9584bb97095e2a6985ab31452732c22
oeyoews <jyao4783@gmail.com>
2023-01-12 15:07:02 +0800

    feat: enable plugin update notify

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -45,6 +45,13 @@ local lazyconfig = {
   dev = {
     path = configdir .. "/builtin",
   },
+  checker = {
+    -- automatically check for plugin updates
+    enabled = true,
+    concurrency = nil, ---@type number? set to 1 to check for updates very slowly
+    notify = true, -- get a notification when new updates are found
+    frequency = 3600, -- check for updates every hour
+  },
 }
 
 -- lazy entry
--- lua/core/pluginlist.lua
+++ lua/core/pluginlist.lua
@@ -266,7 +266,19 @@ oeyoews.pluginlist = {
   },
   {
     "nvim-lualine/lualine.nvim",
-    config = [[require("lualine").setup()]],
+    config = function()
+      require("lualine").setup({
+        sections = {
+          lualine_x = {
+            {
+              require("lazy.status").updates,
+              cond = require("lazy.status").has_updates,
+              color = { fg = "#ff9e64" },
+            },
+          },
+        },
+      })
+    end,
   },
 }
 
