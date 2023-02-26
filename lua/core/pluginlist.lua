752c0b13062328dd6adc0e5a47794af809433d35
oeyoews <jyao4783@gmail.com>
2023-02-26 17:25:01 +0800

    Automatic commit

--- lua/core/pluginlist.lua
+++ lua/core/pluginlist.lua
@@ -6,7 +6,7 @@
 --   By: oeyoews <mail>                                                       --
 --                                                                            --
 --   Created: 2023/01/12 21:24:23 by oeyoews                                  --
---   Updated: 2023/02/26 12:06:37 by oeyoews                                  --
+--   Updated: 2023/02/26 17:24:44 by oeyoews                                  --
 -- -------------------------------------------------------------------------- --
 
 local builtinDir = vim.fn.stdpath("config") .. "/builtin/"
