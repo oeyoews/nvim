d310522b83044e63dbefb24042f6f828d2aff3af
oeyoews <jyao4783@gmail.com>
2023-04-24 09:48:14 +0800

    Automatic commit

--- lua/modules/tools/lualine.lua
+++ lua/modules/tools/lualine.lua
@@ -1,5 +1,15 @@
+local function get_status_string()
+  return vim.fn["codeium#GetStatusString"]()
+end
+
 require("lualine").setup({
   sections = {
+    lualine_c = {
+      {
+        get_status_string,
+        color = { fg = "#ff9e64" },
+      },
+    },
     lualine_x = {
       {
         require("lazy.status").updates,
